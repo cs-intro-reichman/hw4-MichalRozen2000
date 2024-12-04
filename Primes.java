a0c2eec67bdaa151001e8e7c226696a8e4b086e3
MichalRozen2000 <michal.rozen@post.runi.ac.il>
2024-12-04 20:34:09 +0200

    Add files via upload

--- Primes.java
+++ Primes.java
@@ -1,5 +1,34 @@
+
+
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        final int n = Integer.parseInt(args[0]);
+        boolean[] arr = new boolean[n+1]; 
+        int p = 2;
+        for (int i = 2 ; i <=n ; i++){
+                    arr[i] = true;
+        }
+        while (p*p <= n){
+            if (arr[p]){
+                for (int i = p * p ; i <= n ; i += p){
+                    arr[i] = false;
+                }
+            }
+            p++;
+        }
+            
+        
+        System.out.println("Prime numbers up to " + n + ":");
+        int counter = 0;
+        for (int i = 2 ; i <= n ; i++){
+            if (arr[i]){
+            System.out.println(i + " ");
+            counter++;
+            }
+        }
+        int per = (counter / 100) * n;
+        System.out.println("There are " + counter + "between 2 and " + n + "(" + per + "% are primes)");
+      }
     }
-}
+    
+  
